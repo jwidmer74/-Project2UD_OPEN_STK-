695c22cf79e75a3b20bc62f55919b32e8fab98e2
Joshua Widmer <jakron74@hotmail.com>
2019-04-03 21:24:57 -0700

    lesson working

--- fullstack-nanodegree-vm/vagrant/project/database_setup_employee.py
+++ fullstack-nanodegree-vm/vagrant/project/database_setup_employee.py
@@ -8,25 +8,6 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-class Restaurant(Base):
-
-    __tablename__= 'restaurant'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-
-class MenuItem(Base):
-
-    __tablename__ = 'menu_item'
-
-    name =Column(String(80), nullable = False)
-    id = Column(Integer, primary_key = True)
-    description = Column(String(250))
-    price = Column(String(8))
-    course = Column(String(250))
-    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))  
-    restaurant = relationship(Restaurant)  
-
 class Employee(Base):
     __tablename__= 'employee'
     street = Column(String(80),nullable=False)
@@ -43,12 +24,3 @@ class Address(Base):
     engine = create_engine('sqlite:///employeeData.db')
     Base.metadata.create_all(engine)
 
-
-
-
-
-####insert at end of file################
-
-
-engine = create_engine( 'sqlite:///restaurantmenu.db')
-Base.metadata.create_all(engine)
