6e9bab01f9a61ece94e1798d31867bfa12a62628
Joshua Widmer <jakron74@hotmail.com>
2019-04-07 09:11:09 -0700

    added files from Max

--- fullstack-nanodegree-vm/vagrant/project/database_setup.py
+++ fullstack-nanodegree-vm/vagrant/project/database_setup.py
@@ -1,6 +1,5 @@
 import os
 import sys
-
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
@@ -8,47 +7,46 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-class Restaurant(Base):
 
-    __tablename__= 'restaurant'
+class Restaurant(Base):
+    __tablename__ = 'restaurant'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
-class MenuItem(Base):
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
 
+
+class MenuItem(Base):
     __tablename__ = 'menu_item'
 
-    name =Column(String(80), nullable = False)
-    id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
     description = Column(String(250))
     price = Column(String(8))
     course = Column(String(250))
-    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))  
-    restaurant = relationship(Restaurant)  
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant)
 
-class Employee(Base):
-    __tablename__= 'employee'
-    street = Column(String(80),nullable=False)
-    id = Column(Integer, primary_key=True)
-
-class Address(Base):
-    __tablename__= 'address'
-    street =Column(String(80), nullable = False)
-    zip = Column(String(5), nullable = False)
-    id = Column(Integer, primary_key=True)
-    employee_id = Column(Integer,ForeignKey('employee.id'))
-    employee = relationship(Employee)
-
-    engine = create_engine('sqlite:///employeeData.db')
-    Base.metadata.create_all(engine)
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'price': self.price,
+            'course': self.course,
+        }
 
 
+engine = create_engine('sqlite:///restaurantmenu.db')
 
 
-
-####insert at end of file################
-
-
-engine = create_engine( 'sqlite:///restaurantmenu.db')
 Base.metadata.create_all(engine)
